ff8b77a34b2c8f16d76adf4ed6903e951e70f29f
Charlie Morris <cdm32@psu.edu>
2018-12-03 16:22:09 -0500

    Testing traject build/test stage

---
+++ lib/traject/psulib_config.rb
@@ -0,0 +1,285 @@
+# PSU Library MARC to Solr indexing
+# Uses traject: https://github.com/traject-project/traject
+
+require 'bundler/setup'
+require 'library_stdnums'
+is_jruby = RUBY_ENGINE == 'jruby'
+require 'traject/marc4j_reader' if is_jruby
+require 'traject/macros/marc21_semantics'
+require 'traject/macros/marc_format_classifier'
+
+extend Traject::Macros::Marc21Semantics
+extend Traject::Macros::MarcFormats
+
+Marc21 = Traject::Macros::Marc21
+MarcExtractor = Traject::MarcExtractor
+
+# Add lib directory to the ruby load path
+$LOAD_PATH.unshift "#{File.dirname(__FILE__)}/lib"
+
+ATOZ = ('a'..'z').to_a.join('')
+ATOU = ('a'..'u').to_a.join('')
+
+settings do
+  # Where to find solr server to write to
+  provide 'solr.url', 'http://localhost:8983/solr/blacklight-core'
+  provide 'log.batch_size', 100_000
+  # set this to be non-negative if threshold should be enforced
+  # provide 'solr_writer.max_skipped', -1
+
+  # solr.version doesn't currently do anything, but set it
+  # anyway, in the future it will warn you if you have settings
+  # that may not work with your version.
+  provide 'solr.version', '7.4.0'
+
+  # Where to send logging
+  provide 'log.file', 'log/traject.log'
+  provide 'log.error_file', 'log/traject_error.log'
+
+  if is_jruby
+    provide 'reader_class_name', 'Traject::Marc4JReader'
+    provide 'marc4j_reader.permissive', true
+    provide 'marc4j_reader.source_encoding', 'UTF-8'
+    # defaults to 1 less than the number of processors detected on your machine
+    # provide 'processing_thread_pool', 7
+    provide 'solrj_writer.commit_on_close', 'true'
+  end
+end
+
+logger.info RUBY_DESCRIPTION
+
+to_field 'id', extract_marc('001', first: true)
+
+to_field 'marc_display_ss', serialized_marc(format: 'xml', allow_oversized: true)
+
+to_field 'all_text_timv', extract_all_marc_values do |_r, acc|
+  acc.replace [acc.join(' ')] # turn it into a single string
+end
+
+to_field 'language_facet_ssim', marc_languages('008[35-37]:041a:041d:')
+to_field 'format', marc_formats
+
+to_field 'isbn_ssim', extract_marc('020a', separator: nil) do |_rec, acc|
+  orig = acc.dup
+  acc.map! { |x| StdNum::ISBN.allNormalizedValues(x) }
+  acc << orig
+  acc.flatten!
+  acc.uniq!
+end
+
+to_field 'material_type_display_ssm', extract_marc('300a', trim_punctuation: true)
+
+# Title fields
+#
+# 245 - main title
+# 130 / 240 / 730 - uniform title (a standardized form of the title with different intensities)
+# 210 - abbreviated title
+# 222 - key title
+# 242 - translation of title by cataloging agency
+# 246 - sub/alternate titles
+# 247 - previous titles
+# 700 - added entry name (not only title-related subfields should be displayed/etc.)
+# 710 - added entry corporate names (same as above)
+# 711 - added entry meeting name (same as above)
+# 740 - uncontrolled/alternate title
+#
+## Title Search Fields
+to_field 'title_tsim', extract_marc('245a')
+to_field 'title_245ab_tsim', extract_marc('245ab', trim_punctuation: true)
+to_field 'title_addl_tsim', extract_marc(%W[
+  245abnps
+  130#{ATOZ}
+  240abcdefgklmnopqrs
+  210ab
+  222ab
+  242abnp
+  243abcdefgklmnopqrs
+  246abcdefgnp
+  247abcdefgnp
+].join(':'))
+to_field 'title_added_entry_tsim', extract_marc(%w[
+  700gklmnoprst
+  710fgklmnopqrst
+  711fgklnpst
+  730abcdefgklmnopqrst
+  740anp
+].join(':'))
+to_field 'title_related_tsim', extract_marc(%w[
+  505t
+  700lktmnoprs
+  710lktmnoprs
+  711lktmnoprs
+  730adfgklmnoprst
+  740anp
+  760st
+  762st
+  765st
+  767st
+  770st
+  772st
+  773st
+  774st
+  775st
+  776st
+  777st
+  780st
+  785st
+  786st
+  787st
+  790lktmnoprs
+  791lktmnoprs
+  792lktmnoprs
+  793adflktmnoprs
+  796lktmnoprs
+  797lktmnoprs
+  798lktmnoprs
+  799alktmnoprs
+].join(':'), trim_punctuation: true) do |record, accumulator|
+  accumulator.each { |value| value.chomp!(' --') } unless record.fields('505').empty?
+end
+
+## Title Display Fields
+to_field 'title_latin_display_ssm', extract_marc('245abcfgknps', alternate_script: false, trim_punctuation: true)
+to_field 'title_vern', extract_marc('245abcfgknps', alternate_script: :only, trim_punctuation: true)
+# use vern title as title_display_ssm if exists
+# otherwise use latin character title as title_display_ssm
+each_record do |_record, context|
+  title_latin = context.output_hash['title_latin_display_ssm']
+  title_vern = context.output_hash['title_vern']
+  if title_vern.nil?
+    context.output_hash['title_display_ssm'] = title_latin
+    # remove duplicate latin title
+    context.output_hash.delete('title_latin_display_ssm')
+  else
+    context.output_hash['title_display_ssm'] = title_vern
+    # remove duplicate vern title
+    context.output_hash.delete('title_vern')
+  end
+end
+to_field 'uniform_title_display_ssm', extract_marc('130adfklmnoprs:240adfklmnoprs:730ai', trim_punctuation: true)
+to_field 'additional_title_display_ssm', extract_marc('210ab:246iabfgnp:247abcdefgnp', trim_punctuation: true)
+to_field 'related_title_display_ssm', extract_marc('700ilktmnoprs3:710ilktmnoprs3:711ilktmnoprs3:730adfgiklmnoprst3:740anp', trim_punctuation: true)
+
+## Title Sort Fields
+to_field 'title_ssort', marc_sortable_title
+
+# Author fields
+## Primary author
+to_field 'author_tsim', extract_marc('100aqbcdk:110abcdfgkln:111abcdfgklnpq')
+
+## Additional authors
+to_field 'author_addl_tsim', extract_marc('700aqbcdk:710abcdfgkln:711abcdfgklnpq')
+
+## Authors for faceting
+to_field 'all_authors_facet_ssim', extract_marc('100aqbcdkj:110abcdfgklnj:111abcdfgklnpqj:700aqbcdjk:710abcdfgjkln:711abcdfgjklnpq', trim_punctuation: true)
+
+## Author display
+to_field 'author_person_display_ssm', extract_marc('100aqbcdkj', trim_punctuation: true)
+to_field 'author_corp_display_ssm', extract_marc('110abcdfgklnj', trim_punctuation: true)
+to_field 'author_meeting_display_ssm', extract_marc('111abcdfgklnpqj', trim_punctuation: true)
+to_field 'addl_author_display_ssm', extract_marc('700aqbcdjk:710abcdfgjkln:711abcdfgjklnpq', trim_punctuation: true)
+
+## Author sorting field
+to_field 'author_ssort', marc_sortable_author
+
+# Subject fields
+## Primary subject
+to_field 'subject_tsim', extract_marc(%W[
+  600#{ATOU}
+  610#{ATOU}
+  611#{ATOU}
+  630#{ATOU}
+  650abcde
+  651ae
+  653a:654abcde:655abc
+].join(':'))
+
+## Additional subject fields
+to_field 'subject_addl_tsim', extract_marc('600vwxyz:610vwxyz:611vwxyz:630vwxyz:650vwxyz:651vwxyz:654vwxyz:655vwxyz')
+to_field 'subject_topic_facet_ssim', extract_marc('600|*0|abcdq:610|*0|ab:611|*0|ab:630|*0|ab:650|*0|a:653|*0|a', trim_punctuation: true) do |record, accumulator, _context|
+  # Include Fast Headings
+  MarcExtractor.new('650|*7|2').collect_matching_lines(record) do |field, _spec, _extractor|
+    if field['2'].to_s.downcase.include? 'fast'
+      fast_subject = Marc21.trim_punctuation field['a']
+      accumulator << fast_subject unless fast_subject.nil?
+    end
+  end
+  accumulator.compact!
+  accumulator.uniq!
+end
+
+# Publication fields
+to_field 'published_display_ssm', extract_marc('260a', trim_punctuation: true, alternate_script: false)
+to_field 'published_vern_display_ssm', extract_marc('260a', trim_punctuation: true, alternate_script: :only)
+to_field 'pub_date_ssim', marc_publication_date
+
+# Series fields
+#
+# Series Titles
+#
+# Series titles can cause some confusion, as they may contain keywords which aren't necessarily related to the series.
+# For example, "Penguin History of Britain" will return in a title search for "Penguin" if it's part of the title search.
+#
+# 490a - the title of a series.
+# 440a - deprecated same as 490a. We still have a lot of these, though, so if we index 490, we should index 440.
+#
+# Note: 400/410/411 subfield t were deprecated but we still have indexing set up for them
+to_field 'series_title_tsim', extract_marc('440anpv:490av')
+to_field 'series_title_display_ssm', extract_marc('490avlx3:440anpvx', alternate_script: false, trim_punctuation: true)
+
+# Call Number fields
+to_field 'lc_callnum_display_ssm', extract_marc('050ab', first: true)
+to_field 'lc_1letter_facet_sim', extract_marc('050ab', first: true, translation_map: 'callnumber_map') do |_rec, acc|
+  # Just get the first letter to send to the translation map
+  acc.map! { |x| x[0] }
+end
+
+alpha_pat = /\A([A-Z]{1,3})\d.*\Z/
+to_field 'lc_alpha_facet_sim', extract_marc('050a', first: true) do |_rec, acc|
+  acc.map! do |x|
+    (m = alpha_pat.match(x)) ? m[1] : nil
+  end
+  acc.compact! # eliminate nils
+end
+
+to_field 'lc_b4cutter_facet_sim', extract_marc('050a', first: true)
+
+# URL fields
+notfulltext = /abstract|description|sample text|table of contents|/i
+
+to_field('url_fulltext_display_ssm') do |rec, acc|
+  rec.fields('856').each do |f|
+    case f.indicator2
+    when '0'
+      f.find_all { |sf| sf.code == 'u' }.each do |url|
+        acc << url.value
+      end
+    when '2'
+      # do nothing
+    else
+      z3 = [f['z'], f['3']].join(' ')
+      unless notfulltext.match(z3)
+        acc << f['u'] unless f['u'].nil?
+      end
+    end
+  end
+end
+
+# Very similar to url_fulltext_display_ssm. Should DRY up.
+to_field 'url_suppl_display_ssm' do |rec, acc|
+  rec.fields('856').each do |f|
+    case f.indicator2
+    when '2'
+      f.find_all { |sf| sf.code == 'u' }.each do |url|
+        acc << url.value
+      end
+    when '0'
+      # do nothing
+    else
+      z3 = [f['z'], f['3']].join(' ')
+      if notfulltext.match(z3)
+        acc << f['u'] unless f['u'].nil?
+      end
+    end
+  end
+end
